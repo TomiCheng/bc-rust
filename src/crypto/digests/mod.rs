e89ee543ac078950ad143dfbc2680c5a6f91be36
Tomi <hungshou.cheng@iisigroup.com>
2025-10-02 10:27:38 +0800

    feat: expose digest modules and update module visibility

--- src/crypto/digests/mod.rs
+++ src/crypto/digests/mod.rs
@@ -1,5 +1,9 @@
 mod general_digest;
 mod test_digest;
-pub mod sha1_digest;
-pub mod sha256_digest;
+mod sha1_digest;
+mod sha256_digest;
 mod md2_digest;
+
+pub use sha1_digest::Sha1Digest;
+pub use sha256_digest::Sha256Digest;
+pub use md2_digest::Md2Digest;
